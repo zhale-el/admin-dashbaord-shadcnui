9e45a3622abc1225e9374a086b4a31c008ce3fa8
zhaleh <zhalehelmi@gmail.com>
2024-06-21 14:46:36 +0330

    create dashboard component and card

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,32 @@
+import DashboardCard from "@/components/dashboard/DashboardCard";
+import { Newspaper, Folder, User, MessageCircle } from "lucide-react";
+
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-2xl">Dashbaord</h1>
-    </div>
+    <>
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+        <DashboardCard
+          title="Posts"
+          count={100}
+          icon={<Newspaper className="text-slate-500 " size={72} />}
+        />
+        <DashboardCard
+          title="Categories"
+          count={12}
+          icon={<Folder className="text-slate-500 " size={72} />}
+        />
+        <DashboardCard
+          title="Users"
+          count={750}
+          icon={<User className="text-slate-500 " size={72} />}
+        />
+        <DashboardCard
+          title="Comments"
+          count={1200}
+          icon={<MessageCircle className="text-slate-500 " size={72} />}
+        />
+      </div>
+    </>
   );
 };
 
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -15,7 +15,7 @@ import {
   Folders,
   CreditCard,
   Settings,
-  Users,
+  User,
 } from "lucide-react";
 import Link from "next/link";
 const Sidebar = () => {
@@ -42,9 +42,21 @@ const Sidebar = () => {
         </CommandGroup>
         <CommandSeparator />
         <CommandGroup heading="Settings">
-          <CommandItem>Profile</CommandItem>
-          <CommandItem>Billing</CommandItem>
-          <CommandItem>Settings</CommandItem>
+          <CommandItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard className="mr-2 h-4 w-4" />
+            <span>Billing</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
         </CommandGroup>
       </CommandList>
     </Command>
