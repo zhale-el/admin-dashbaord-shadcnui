2f1a4dbaa4b15273459a87c6f010354e9fe45243
zhaleh <zhalehelmi@gmail.com>
2024-06-29 12:15:42 +0330

    add form

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,10 +1,13 @@
 import PostsTabel from "@/components/posts/PostsTabel";
 import BackButton from "@/components/BackButton";
+import PostsPagination from "@/components/posts/PostsPagination";
+
 const PostsPage = () => {
   return (
     <>
       <BackButton text="Go Back" link="/" />
       <PostsTabel />
+      <PostsPagination />
     </>
   );
 };
